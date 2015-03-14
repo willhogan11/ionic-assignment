705cfd698daefcb4699d8e8adfe8ec12eff521e3
Will Hogan <willhogan11@hotmail.com>
2015-03-14 23:36:29 +0000

    Version 2, Branch 1

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,9 @@
-angular.module('calorific', ['ionic'])
+angular.module('"calorieManagementApp"', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function($ionicPlatform)
+{
+  $ionicPlatform.ready(function()
+{
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -12,3 +14,29 @@ angular.module('calorific', ['ionic'])
     }
   });
 })
+
+
+// States Declaration
+.config(function($stateProvider, $urlRouterProvider)
+{
+        $urlRouterProvider.otherwise('/');
+
+        // List of States
+        $stateProvider
+
+        .state('menuState',
+        {
+          url: '/',
+          templateUrl: 'menuState.html'
+        })
+
+        .state('nextState',
+        {
+          url: '/nextState',
+		  templateUrl: 'nextState.html'
+        });
+
+}) // End of States Declaration
+
+
+                                                                
